10cc3ed5c8294a520ed63e0d05911520dbd18e36
biplavgkv <79261429+biplavgkv@users.noreply.github.com>
2023-07-05 18:20:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,1 @@
+apikey = "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"
---
+++ main.py
@@ -0,0 +1,131 @@
+import speech_recognition as sr
+import os
+import win32com.client
+speaker=win32com.client.Dispatch("SAPI.SpVoice")
+
+def say(text):
+    speaker.Speak(text)
+    return
+
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+from config import apikey
+import datetime
+
+
+
+chatStr = ""
+# https://youtu.be/Z3ZAJoi4x6Q
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = apikey
+    chatStr += f"Rishu: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt= chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+    return response["choices"][0]["text"]
+
+
+def ai(prompt):
+    openai.api_key = apikey
+    text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+
+    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+
+def say(text):
+    speaker.Speak(text)
+    return
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
+
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["cricbuzz", "https://www.cricbuzz.com"],["cricbuzz", "https://www.cricbuzz.com"],["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # odo: Add a feature to play a specific song
+        if "open music" in query:
+            print("music")
+            musicPath = "file:///C:/Users/bkumar/Music/Krishna.mp3"
+            webbrowser.open(musicPath)
+
+
+        elif "the time" in query:
+
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} bajke {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            webbrowser.open(f"open /System/Applications/FaceTime.app")
+
+        elif "open pass".lower() in query.lower():
+            webbrowser.open(f"open /Applications/Passky.app")
+
+        elif "hello ai".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            #chat(query)
+
+
+
+
+
+        # say(query)
